1f1ebd4a56ba1d5a94a56b20fcdefdde8376a52c
Slavomir Vojacek <slavomirvojacek@gmail.com>
2017-05-22 12:20:11 +0100

    Fill all diagonal pairs correctly

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,15 @@ fn main() {
 	board = fill_row(board, queen_position);
 	board = fill_col(board, queen_position);
 
+	for position in diagonals_to_fill {
+		board = fill_pos(board, position);
+	}
+
 	for r in board {
 		println!("{:?}", r);
 	}
 
-	println!("{:?}", diagonals_to_fill);
+	// println!("{:?}", diagonals_to_fill);
 }
 
 fn place_queen(mut board: Board, pos: (u8, u8)) -> Board {
@@ -31,11 +35,14 @@ fn place_queen(mut board: Board, pos: (u8, u8)) -> Board {
 	board
 }
 
-// fn fill_pos(mut board: Board, pos: (u8, u8)) -> Board {
-// 	let (r, c) = pos;
-// 	board[r as usize][c as usize] = Square::Unattainable;
-// 	board
-// }
+fn fill_pos(mut board: Board, pos: (u8, u8)) -> Board {
+	let (r, c) = pos;
+	match board[r as usize][c as usize] {
+		Square::Empty => board[r as usize][c as usize] = Square::Unattainable,
+		_ => panic!("Unexpected Queen"),
+	}
+	board
+}
 
 fn fill_row(mut board: Board, pos: (u8, u8)) -> Board {
 	let (r, c) = pos;
@@ -47,7 +54,7 @@ fn fill_row(mut board: Board, pos: (u8, u8)) -> Board {
 					_ => match col_index == c as usize {
 						true => {},
 						_ => panic!("Unexpected Queen"),
-					} 
+					},
 				}
 			}
 		}
@@ -65,7 +72,7 @@ fn fill_col(mut board: Board, pos: (u8, u8)) -> Board {
 					_ => match col_index == c as usize {
 						true => {},
 						_ => panic!("Unexpected Queen"),
-					}
+					},
 				}
 			}
 		}
@@ -113,183 +120,3 @@ fn get_diagonals_to_fill(size: u8, pos: (u8, u8)) -> Vec<(u8, u8)> {
 fn can_be_col_index(size: u8, col: i8) -> bool {
 	col <= size as i8 && col >= 0
 }
-
-
-
-
-
-// .iter_mut()
-		// .map(|s| match s {
-		// 	&mut Square::Empty => Square::Unattainable,
-		// 	_ => *s,
-		// })
-		// .collect();
-
-// fn fill_col_at(mut board: Vec<Vec<Square>>, r: usize, i: usize) -> Vec<Vec<Square>> {
-// 	for index in 0..board[r].len() {
-// 		if (index == i) && (board[r][index] == Square::Empty) {
-// 			board[r][index] = Square::Unattainable;
-// 		};
-// 	};
-
-// 	board
-// }
-
-// fn fill_row(mut row: Vec<Square>) -> Vec<Square> {
-// 	row
-// 		.iter_mut()
-// 		.map(|s| match s {
-// 			&mut Square::Empty => Square::Unattainable,
-// 			_ => *s,
-// 		})
-// 		.collect()
-// }
-
-// fn can_place_queen(board: &Board, row: u8, column: u8) -> Result<(usize, usize), &str> {
-// 	match board[row as usize][column as usize] {
-// 		Square::Empty => Ok((row as usize, column as usize)),
-// 		Square::Unattainable => Err("R{} C{} is unattainable"),
-// 		_ => Err("R{} C{} is already occupied by a Queen"),
-// 	}
-// }
-
-// fn place_queen(mut board: Board, r: u8, c: u8) {
-// 	if let Ok((row, col)) = can_place_queen(&board, r, c) {
-// 		board[row][col] = Square::Queen { r: r, c: c };
-// 		board = fill_unattainable_row(board, row);
-// 		// board = fill_unattainable_column(board, col);
-// 		// board = fill_unattainable_diagonal_bottom_top(board, row, col);
-// 		// board = fill_unattainable_diagonal_top_bottom(board, row, col);
-// 		for r in board.iter() {
-// 			println!("{:?}", r);
-// 		}
-// 	} else {
-
-// 	}
-// }
-
-// fn fill_unattainable_row(board: Board, row: usize) -> Board {
-// 	let mut i: usize = 0;
-	
-// 	board
-// 		.iter()
-// 		.map(|r| match i {
-// 			row => {
-// 				i += 1;
-				
-// 				r
-// 					.iter()
-// 					.map(|c| match c {
-// 						&Square::Empty => Square::Unattainable,
-// 						_ => *c,
-// 					})
-// 			},
-// 			_ => r,
-// 		}).collect();
-
-
-// 	// board[r].iter_mut().map(|x| match x {
-// 	// 		&mut Square::Empty => Square::Unattainable,
-// 	// 		_ => *x,
-// 	// 	});
-
-// 	board
-// }
-
-// fn fill_unattainable_column(board: [[Square; 8]; 8], c: usize) -> [[Square; 8]; 8] {
-// 	let mut board = board.clone();
-// 	for i in 0..board.len() {
-// 		match board[i][c] {
-// 			Square::Empty => board[i][c] = Square::Unattainable,
-// 			_ => {}
-// 		}
-// 	}
-
-// 	board
-// }
-
-// fn fill_unattainable_diagonal_bottom_top(board: [[Square; 8]; 8], r: usize, c: usize) -> [[Square; 8]; 8] {
-// 	let mut board = board.clone();
-	
-// 	let start_row = r.clone();
-// 	let start_column = c.clone();
-// 	let board_len = board.len();
-
-// 	let mut current_column = &start_column - 0;
-
-// 	// for i in start_row..board_len {
-// 	// 	if (i == start_row) {
-// 	// 		// ignore
-// 	// 	} else {
-// 	// 		if (current_column >= 0) {
-// 	// 			match board[i][current_column] {
-// 	// 				Square::Empty => board[i][current_column] = Square::Unattainable,
-// 	// 				_ => {}
-// 	// 			}
-// 	// 		}
-// 	// 	}
-
-// 	// 	current_column -= 1;
-// 	// }
-
-// 	current_column = &start_column + 0;
-
-// 	for i in (0..start_row).rev() {
-// 		if (i == start_row) {
-// 			// ignore
-// 		} else {
-// 			if (current_column <= board_len) {
-// 				match board[i][current_column] {
-// 					Square::Empty => board[i][current_column] = Square::Unattainable,
-// 					_ => {}
-// 				}
-// 			}
-// 		}
-
-// 		current_column += 1;
-// 	}
-
-// 	board
-// }
-
-// fn fill_unattainable_diagonal_top_bottom(board: [[Square; 8]; 8], r: usize, c: usize) -> [[Square; 8]; 8] {
-// 	let mut board = board.clone();
-	
-// 	let start_row = r.clone();
-// 	let start_column = c.clone();
-// 	let board_len = board.len();
-	
-// 	let mut current_column = &start_column + 1;
-
-// 	for i in start_row..board_len {
-// 		if (i == start_row || current_column > board_len) {
-// 			// ignore
-// 		} else {
-// 			// match board[i][current_column] {
-// 			// 	Square::Empty => board[i][current_column] = Square::Unattainable,
-// 			// 	_ => {}
-// 			// };
-
-// 			// current_column += 1;
-// 		}
-// 	}
-
-// 	current_column = &start_column - 1;
-
-// 	for i in (0..start_row).rev() {
-// 		if (i == start_row || current_column < 0) {
-// 			// ignore
-// 		} else {
-// 			// match board[i][current_column] {
-// 			// 	Square::Empty => board[i][current_column] = Square::Unattainable,
-// 			// 	_ => {}
-// 			// };
-
-// 			// if (current_column != 0) {
-// 			// 	current_column -= 1;
-// 			// }
-// 		}
-// 	}
-
-// 	board
-// }
